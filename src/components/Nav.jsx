f582628df3c4090b8633afc0a1845d31b6582476
Maksym Durikhin <max1mkasss.mail@gmail.com>
2025-12-16 21:58:45 +0200

    vers. 0.002, navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,39 @@
 import '../styles/nav.css';
 
 function Nav() {
-  return; 
+	return (
+		<nav>
+			<ul className="links-list">
+				<li>
+					<a href="#" className="links-nav">
+						Blog
+					</a>
+				</li>
+				<li>
+					<a href="#" className="links-nav">
+						About
+					</a>
+				</li>
+			</ul>
+
+			<svg
+				width="23"
+				height="23"
+				viewBox="0 0 23 23"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M16.864 2.74285C20.7917 6.46981 21.0348 12.6476 17.4121 16.6716L21.9172 21.1619C22.1157 21.3673 22.1128 21.6939 21.9108 21.8959C21.7089 22.0979 21.3823 22.1007 21.1768 21.9023L16.6716 17.4121C12.6476 21.0348 6.46981 20.7917 2.74285 16.864C-0.984114 12.9363 -0.903064 6.75424 2.92559 2.92559C6.75424 -0.903064 12.9363 -0.984114 16.864 2.74285ZM1.125 10.028C1.125 14.9444 5.11048 18.9299 10.0268 18.9299C14.9408 18.9241 18.9229 14.942 18.9286 10.028C18.9286 5.1117 14.9432 1.12622 10.0268 1.12622C5.11048 1.12622 1.125 5.1117 1.125 10.028Z"
+					fill="#333333"
+				/>
+			</svg>
+
+            <button className='constactUs-button'>Contact Us</button>
+		</nav>
+	);
 }
 
 export default Nav;
